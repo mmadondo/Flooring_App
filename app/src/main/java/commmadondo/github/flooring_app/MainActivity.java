32639715860f52fc70a57347c75e219c734312bc
mmadondo <madondomalvern@gmail.com>
2017-03-06 19:24:05 -0600

    Flooring Area Calculator

--- app/src/main/java/commmadondo/github/flooring_app/MainActivity.java
+++ app/src/main/java/commmadondo/github/flooring_app/MainActivity.java
@@ -4,16 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etLength, etWidth;
+    Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etLength = (EditText) findViewById(R.id.editTextLength);
+        etWidth = (EditText) findViewById(R.id.editTextWidth);
+        btnCalculate = (Button) findViewById(R.id.btnResults);
     }
-    public void buttonClick(View view){
+
+    public void onClickResult(View view){
         Intent calcIntent = new Intent(MainActivity.this, SecondActivity.class);
+        calcIntent.putExtra("rmLength", Double.parseDouble(etLength.getText().toString()));
+        calcIntent.putExtra("rmWidth", Double.parseDouble(etWidth.getText().toString()));
         startActivity(calcIntent);
     }
+
 }
--- app/src/main/java/commmadondo/github/flooring_app/SecondActivity.java
+++ app/src/main/java/commmadondo/github/flooring_app/SecondActivity.java
@@ -2,15 +2,31 @@ package commmadondo.github.flooring_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by mmadondo on 3/3/2017.
  */
 
-public class SecondActivity extends AppCompatActivity{
+public class SecondActivity extends AppCompatActivity {
+
+    static TextView tvResult;
+    Double Length, Width, Area;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        tvResult = (TextView) findViewById(R.id.textViewResult);
+
+        Bundle extras = getIntent().getExtras();
+        Length = extras.getDouble("rmLength");
+        Width = extras.getDouble("rmWidth");
+
+        Area = Length*Width;
+        tvResult.setText("Width is " + Width + " and Length is " + Length + " and flooring needed is " + Area);
     }
-}
+
+    }
+
